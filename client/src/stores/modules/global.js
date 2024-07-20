e9e550e2422b5cd3cf09ce9205c1363bca427243
codergb <betweent@126.com>
2024-07-20 16:03:25 +0800

    主题切换

--- client/src/stores/modules/global.js
+++ client/src/stores/modules/global.js
@@ -1,9 +1,17 @@
 import { defineStore } from "pinia";
+import { DEFAULT_PRIMARY } from "@/config";
 export const useGlobalStore = defineStore({
   id: "admin-global",
   state: () => ({
     isCollapse: false,
-    breadcrumbIcon: true
+    breadcrumbIcon: true,
+    // 深色模式
+    isDark: false,
+    // 灰色模式
+    isGrey: false,
+    // 色弱模式
+    isWeak: false,
+    primary: DEFAULT_PRIMARY,
   }),
   actions: {
     setGlobalState(...args) {

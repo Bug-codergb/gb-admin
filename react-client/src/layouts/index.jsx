bf0dd1c137bb591a6cf08376a4c75177ffff3553
codergb <betweent@126.com>
2024-08-10 16:23:54 +0800

    水印

--- react-client/src/layouts/index.jsx
+++ react-client/src/layouts/index.jsx
@@ -1,12 +1,15 @@
 import React, { memo } from "react";
+import { Watermark } from "antd";
 import LayoutVerical from "./LayoutVerical/index";
 import ThemeDrawer from "./components/ThemeDrawer";
 import * as style from "./style.module.scss";
 const Layouts = () => {
   return (
     <div className={style["layout-container"]}>
-      <LayoutVerical />
-      <ThemeDrawer />
+      <Watermark content="Admin" className={style["watermark-container"]}>
+        <LayoutVerical />
+        <ThemeDrawer />
+      </Watermark>
     </div>
   );
 };

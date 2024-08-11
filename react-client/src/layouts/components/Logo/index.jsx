81385b5666f7fd21b35fe1fca74ea84592608ed1
codergb <betweent@126.com>
2024-08-11 21:01:49 +0800

    首页

--- react-client/src/layouts/components/Logo/index.jsx
+++ react-client/src/layouts/components/Logo/index.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import * as styles from "./style.module.scss";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 const Logo = () => {
   const isCollapse = useSelector(state => state.global.isCollapse);
+  const navigate = useNavigate();
+  const handleHomeRouter = () => {
+    navigate("/data");
+  };
   return (
-    <div className={styles["logo-container"]}>
+    <div className={styles["logo-container"]} onClick={handleHomeRouter}>
       <img src="/logo.svg" width="40" />
       {!isCollapse && <div className={`text ${styles["logo-text"]}`}>Admin</div>}
     </div>

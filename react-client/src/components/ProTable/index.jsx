7adc64d78ba0cfb6f0888a48c523d307ff90d65b
codergb <betweent@126.com>
2024-08-12 20:28:23 +0800

    优化table

--- react-client/src/components/ProTable/index.jsx
+++ react-client/src/components/ProTable/index.jsx
@@ -1,31 +1,32 @@
 import React, { memo } from "react";
 import { Table } from "antd";
-import { Input, Pagination } from "antd";
+import { Input, Pagination, Button } from "antd";
 import * as styles from "./style.module.scss";
 const columns = [
   {
     title: "姓名",
     dataIndex: "name",
     key: "name",
-    with: 200
+    with: 300,
+    fixed: "left"
   },
   {
     title: "年龄",
     dataIndex: "age",
     key: "age",
-    with: 200
+    with: 300
   },
   {
     title: "住址",
     dataIndex: "address",
     key: "address",
-    with: 200
+    with: 400
   },
   {
     title: "姓名1",
     dataIndex: "name11",
     key: "name11",
-    with: 200
+    with: 400
   },
   {
     title: "年龄12",
@@ -43,8 +44,7 @@ const columns = [
   {
     title: "年龄123",
     dataIndex: "age322",
-    key: "age223",
-    with: 200
+    key: "age223"
   },
   {
     title: "年龄1231",
@@ -61,14 +61,21 @@ const columns = [
   {
     title: "年2龄1231",
     dataIndex: "ag2e3222",
-    key: "age22233",
+    key: "age22233111",
+    with: 500
+  },
+  {
+    title: "年2龄dd1231",
+    dataIndex: "ag2edd3222",
+    key: "age22ddd233",
     with: 200
   },
   {
     title: "住址2131113",
     dataIndex: "address133",
     key: "address133",
-    fixed: "right"
+    fixed: "right",
+    with: 100
   }
 ];
 const dataSource = [
@@ -86,49 +93,49 @@ const dataSource = [
     address: "西湖区湖底公园1号"
   },
   {
-    key: "1",
+    key: "3",
     name: "胡彦斌",
     age: 32,
     address: "西湖区湖底公园1号"
   },
   {
-    key: "2",
+    key: "4",
     name: "胡彦祖",
     age: 42,
     address: "西湖区湖底公园1号"
   },
   {
-    key: "1",
+    key: "5",
     name: "胡彦斌",
     age: 32,
     address: "西湖区湖底公园1号"
   },
   {
-    key: "2",
+    key: "6",
     name: "胡彦祖",
     age: 42,
     address: "西湖区湖底公园1号"
   },
   {
-    key: "1",
+    key: "7",
     name: "胡彦斌",
     age: 32,
     address: "西湖区湖底公园1号"
   },
   {
-    key: "2",
+    key: "8",
     name: "胡彦祖",
     age: 42,
     address: "西湖区湖底公园1号"
   },
   {
-    key: "1",
+    key: "9",
     name: "胡彦斌",
     age: 32,
     address: "西湖区湖底公园1号"
   },
   {
-    key: "2",
+    key: "10",
     name: "胡彦祖",
     age: 42,
     address: "西湖区湖底公园1号"
@@ -138,10 +145,24 @@ const ProTable = () => {
   return (
     <div className={["table-main"]}>
       <div className={styles["table-header"]}>
-        <Input placeholder="Basic usage" />
+        <div className="lf">
+          <Input placeholder="Basic usage" />
+        </div>
+        <div className="rt">
+          <Button type="primary">确定</Button>
+        </div>
       </div>
       <div className="el-table">
-        <Table dataSource={dataSource} columns={columns} pagination={false} scroll={{ y: "auto" }} />
+        <Table
+          dataSource={dataSource}
+          rowKey="key"
+          scroll={{ y: 100, x: 400 }}
+          bordered={false}
+          size={"middle"}
+          columns={columns}
+          pagination={false}
+          tableLayout="fixed"
+        />
       </div>
       <div className={styles["page-container"]}>
         <Pagination defaultCurrent={1} total={50} />

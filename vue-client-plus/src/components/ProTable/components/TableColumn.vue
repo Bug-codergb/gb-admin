c16368729067e38b283b6cbf92f2ab0c9e7c200e
guobin <guobin@tianchuangsec.com>
2025-01-08 14:03:24 +0800

    proTable

--- vue-client-plus/src/components/ProTable/components/TableColumn.vue
+++ vue-client-plus/src/components/ProTable/components/TableColumn.vue
@@ -4,30 +4,37 @@
 
 <script setup lang="jsx" name="TableColumn">
 import { inject, ref, useSlots } from "vue";
-import { ColumnProps, RenderScope, HeaderRenderScope } from "@/components/ProTable/interface";
+//import { ColumnProps, RenderScope, HeaderRenderScope } from "@/components/ProTable/interface";
 import { filterEnum, formatValue, handleProp, handleRowAccordingToProp } from "@/utils";
 
-defineProps<{ column: ColumnProps }>();
-
+//defineProps<{ column: ColumnProps }>();
+const props= defineProps({
+  column:{
+    type:Object,
+    default(){
+      return {}
+    }
+  }
+})
 const slots = useSlots();
 
 const enumMap = inject("enumMap", ref(new Map()));
 
 // 渲染表格数据
-const renderCellData = (item: ColumnProps, scope: RenderScope<any>) => {
+const renderCellData = (item, scope) => {
   return enumMap.value.get(item.prop) && item.isFilterEnum
-    ? filterEnum(handleRowAccordingToProp(scope.row, item.prop!), enumMap.value.get(item.prop)!, item.fieldNames)
-    : formatValue(handleRowAccordingToProp(scope.row, item.prop!));
+    ? filterEnum(handleRowAccordingToProp(scope.row, item.prop), enumMap.value.get(item.prop), item.fieldNames)
+    : formatValue(handleRowAccordingToProp(scope.row, item.prop));
 };
 
 // 获取 tag 类型
-const getTagType = (item: ColumnProps, scope: RenderScope<any>) => {
+const getTagType = (item, scope) => {
   return (
-    filterEnum(handleRowAccordingToProp(scope.row, item.prop!), enumMap.value.get(item.prop), item.fieldNames, "tag") || "primary"
+    filterEnum(handleRowAccordingToProp(scope.row, item.prop), enumMap.value.get(item.prop), item.fieldNames, "tag") || "primary"
   );
 };
 
-const RenderTableColumn = (item: ColumnProps) => {
+const RenderTableColumn = (item) => {
   return (
     <>
       {item.isShow && (
@@ -37,16 +44,16 @@ const RenderTableColumn = (item: ColumnProps) => {
           showOverflowTooltip={item.showOverflowTooltip ?? item.prop !== "operation"}
         >
           {{
-            default: (scope: RenderScope<any>) => {
+            default: (scope) => {
               if (item._children) return item._children.map(child => RenderTableColumn(child));
               if (item.render) return item.render(scope);
-              if (item.prop && slots[handleProp(item.prop)]) return slots[handleProp(item.prop)]!(scope);
+              if (item.prop && slots[handleProp(item.prop)]) return slots[handleProp(item.prop)](scope);
               if (item.tag) return <el-tag type={getTagType(item, scope)}>{renderCellData(item, scope)}</el-tag>;
               return renderCellData(item, scope);
             },
-            header: (scope: HeaderRenderScope<any>) => {
+            header: (scope) => {
               if (item.headerRender) return item.headerRender(scope);
-              if (item.prop && slots[`${handleProp(item.prop)}Header`]) return slots[`${handleProp(item.prop)}Header`]!(scope);
+              if (item.prop && slots[`${handleProp(item.prop)}Header`]) return slots[`${handleProp(item.prop)}Header`](scope);
               return item.label;
             }
           }}
